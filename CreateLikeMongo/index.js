3074b780686331f70734de5a228f15e4a18e50e3
Steven <clai86@my.bcit.ca>
2021-03-09 11:46:06 -0800

    jwt verification added

--- CreateLikeMongo/index.js
+++ CreateLikeMongo/index.js
@@ -1,4 +1,5 @@
 const { ObjectId, MongoClient } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const MONGODB_URI = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.fmkwb.mongodb.net/AfkAndChillDatabase?retryWrites=true&w=majority`;
 
 let cachedDb = null;
@@ -18,14 +19,85 @@ async function connectToDatabase() {
     return db;
 }
 
+// Verify if the token is undefied or not
+const vertifyToken = (token) => {
+    if (typeof token !== 'undefined') {
+        return true;
+    }
+
+    return false;
+};
+
 exports.handler = async (event, context) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    let userOneId = '';
-    let userTwoId = '';
+    const { token, userTwoId } = event.headers;
+
+    // Verify the jwt token
+    if (!vertifyToken(token)) {
+        return {
+            statusCode: 403,
+            body: JSON.stringify({
+                errorMsg: 'Token is undefined',
+            }),
+        };
+    }
 
     try {
         // Connect to the database
         const db = await connectToDatabase();
+
+        return jwt.verify(token, 'secret', async (err, authData) => {
+            if (err) {
+                return {
+                    statusCode: 403,
+                    body: JSON.stringify({
+                        errorMsg: 'Token not matched',
+                    }),
+                };
+            }
+
+            // Check if both users exist
+            const firstExistedUser = await db
+                .collection('user')
+                .findOne({ cognito_id: authData.client_id });
+            const secondExistedUser = await db
+                .collection('user')
+                .findOne({ _id: userTwoId });
+            if (!firstExistedUser && !secondExistedUser) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        errorMsg: 'User/Users does not exist',
+                    }),
+                };
+            }
+
+            // Check if user one already liked/disliked user two
+            // Check liked
+            const isLiked = firstExistedUser.likes.includes(
+                secondExistedUser._id
+            );
+            if (isLiked) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        errorMsg: 'User is already being liked',
+                    }),
+                };
+            }
+            // Check disliked
+            const isDisliked = firstExistedUser.dislikes.includes(
+                secondExistedUser._id
+            );
+            if (isDisliked) {
+                return {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        errorMsg: 'User is already being disliked',
+                    }),
+                };
+            }
+        });
 
         // Make sure event.headers have the values for user one and two
         if (event.headers) {
