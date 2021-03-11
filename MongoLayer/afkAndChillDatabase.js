184df887d0e80486b5fe125787d4ab849c621f1b
Steven <clai86@my.bcit.ca>
2021-03-11 01:01:35 -0800

    FInished migrating update like lambda function

--- MongoLayer/afkAndChillDatabase.js
+++ MongoLayer/afkAndChillDatabase.js
@@ -1,4 +1,4 @@
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 module.exports = async function () {
     // MongoDB Atalas connection string
@@ -52,7 +52,65 @@ module.exports = async function () {
         }
     }
 
+    // Update like function
+    async function updateLike({ userOneId, userTwoId }) {
+        try {
+            // Check if both users exist
+            const firstExistedUser = await users.findOne({
+                cognito_id: userOneId,
+            });
+            const secondExistedUser = await users.findOne({
+                cognito_id: userTwoId,
+            });
+            if (!firstExistedUser || !secondExistedUser) {
+                throw 'User/Users does not exist';
+            }
+
+            // Check if user one already liked/disliked user two
+            // Check liked
+            const isLiked = await users.findOne({
+                cognito_id: userOneId,
+                likes: { $in: [userTwoId] },
+            });
+            if (isLiked) {
+                throw 'User is already being liked';
+            }
+            // Check disliked
+            const isDisliked = await users.findOne({
+                cognito_id: userOneId,
+                dislikes: { $in: [userTwoId] },
+            });
+            if (isDisliked) {
+                throw 'User is already being disliked';
+            }
+
+            // Insert second user id into first user likes array
+            await users.findOneAndUpdate(
+                { cognito_id: userOneId },
+                { $push: { likes: userTwoId } }
+            );
+
+            // Check if both users like each other
+            // If YES then create a chatbox for them
+            const isLikedBySecondUser = await users.findOne({
+                cognito_id: userTwoId,
+                likes: { $in: [userOneId] },
+            });
+            if (isLikedBySecondUser) {
+                await chatboxes.insertOne({
+                    user_one: userOneId,
+                    user_two: userTwoId,
+                });
+            }
+
+            return;
+        } catch (error) {
+            throw error;
+        }
+    }
+
     return {
         createUser,
+        updateLike,
     };
 };
